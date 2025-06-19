4af0e9cf99acf1d9801b7cf2d8e820410371296b
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-06-18 22:12:42 -0600

    Create an interface login

---
+++ VRPruebaInterfaz/src/main/java/com/example/vrpruebainterfaz/Main.java
@@ -0,0 +1,19 @@
+package com.example.vrpruebainterfaz;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+    public void mostrar(Stage stage) {
+
+    }
+}
