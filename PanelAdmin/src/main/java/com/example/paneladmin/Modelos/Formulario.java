94299983650c5aace12679d1819af63ca7a6da97
Tony <20243ds055@utez.edu.mx>
2025-06-23 22:48:11 -0600

    Constructor agregado en Modelos/Formulario

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Formulario.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Formulario.java
@@ -5,4 +5,10 @@ public class Formulario {
     private String id;
     private String nombre;
     private String estado;
+
+    public Formulario(String id, String nombre, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
 }
