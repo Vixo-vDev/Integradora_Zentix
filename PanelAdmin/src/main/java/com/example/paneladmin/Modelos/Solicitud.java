2c5441f127917c5d4cd34e78de686bdd530c64d8
Tony <20243ds055@utez.edu.mx>
2025-06-23 23:04:56 -0600

    Getters y Setters agregados en Modelos/Solicitud

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Solicitud.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Solicitud.java
@@ -13,4 +13,36 @@ public class Solicitud {
         this.usuario = usuario;
         this.fecha = fecha;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 }
