19e5821d8b82a266929841bcc77669be6012460d
Tony <20243ds055@utez.edu.mx>
2025-06-23 22:28:17 -0600

    Getters y Setters agregados en Modelos/Producto

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Producto.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Producto.java
@@ -11,4 +11,28 @@ public class Producto {
         this.cantidad = cantidad;
         this.precio = precio;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
