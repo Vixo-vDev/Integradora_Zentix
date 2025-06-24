22538dcc8a8c006b74d30417b54ada0e10381712
Tony <20243ds055@utez.edu.mx>
2025-06-23 22:17:02 -0600

    Los archivos de la caperta Modelos fueron reconfiguradas como clases y no interfaces

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Producto.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Producto.java
@@ -1,16 +1,4 @@
 package com.example.paneladmin.Modelos;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Producto extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class Producto {
 }
