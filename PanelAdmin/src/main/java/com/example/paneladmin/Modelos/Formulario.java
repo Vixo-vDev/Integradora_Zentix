dcb639a9581081e6ef76ed8f0589eac4e5704b32
Tony <20243ds055@utez.edu.mx>
2025-06-23 22:52:34 -0600

    Getters y Setters agregados en Modelos/Formulario

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Formulario.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Formulario.java
@@ -11,4 +11,28 @@ public class Formulario {
         this.nombre = nombre;
         this.estado = estado;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
