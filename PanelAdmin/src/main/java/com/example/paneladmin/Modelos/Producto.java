f8ae7805a461d37edf139d5e367602d1a3d5be70
Tony <20243ds055@utez.edu.mx>
2025-06-23 22:27:06 -0600

    Constructor agregados en Modelos/Producto

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Producto.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Producto.java
@@ -6,4 +6,9 @@ public class Producto {
     private int cantidad;
     private double precio;
 
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
 }
