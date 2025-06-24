1ee214724cd6a6bee0dc60f5611cf2f9bb8ef2d6
Tony <20243ds055@utez.edu.mx>
2025-06-23 22:44:31 -0600

    Constructor agregado en Modelos/Usuario

--- PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Usuario.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Modelos/Usuario.java
@@ -5,4 +5,10 @@ public class Usuario {
     private String nombreUsuario;
     private String rol;
     private String correo;
+
+    public Usuario(String nombreUsuario, String rol, String correo) {
+        this.nombreUsuario = nombreUsuario;
+        this.rol = rol;
+        this.correo = correo;
+    }
 }
