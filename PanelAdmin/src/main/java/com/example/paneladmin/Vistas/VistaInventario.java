2a51a226fa090f0dfcbd797180ac13317add3af8
Tony <20243ds055@utez.edu.mx>
2025-06-25 13:35:47 -0600

    VistaInventario ejecutable

--- PanelAdmin/src/main/java/com/example/paneladmin/Vistas/VistaInventario.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Vistas/VistaInventario.java
@@ -1,16 +1,38 @@
 package com.example.paneladmin.Vistas;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import com.example.paneladmin.Controladores.ControladorInventario;
+import com.example.paneladmin.Modelos.Producto;
+import javafx.geometry.Insets;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
-public class VistaInventario extends Application {
+public class VistaInventario {
+    private ControladorInventario controlador;
+    private VBox vista;
+    private TableView<Producto> tabla;
 
-    public static void main(String[] args) {
-        launch(args);
+    public VistaInventario() {
+        vista = new VBox(20);
+        vista.setPadding(new Insets(20));
+        this.controlador = new ControladorInventario(this); // Inicialización correcta del controlador
+        controlador.inicializarUI();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    public VBox getVista() {
+        return vista;
+    }
 
+    public TableView<Producto> getTabla() {
+        return tabla;
+    }
+
+    public void setTabla(TableView<Producto> tabla) {
+        this.tabla = tabla;
+        if (tabla != null) {
+            tabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+            tabla.setStyle("-fx-background-color: white; -fx-border-color: #bdc3c7;");
+            VBox.setVgrow(tabla, Priority.ALWAYS);
+        }
     }
 }
