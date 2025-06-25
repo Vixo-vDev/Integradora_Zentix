55be4dc07e68690e1dd7e16c37b1fa337e399218
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-06-24 23:32:42 -0600

    Creación y programación de botones en registrarse

--- VRPruebaInterfaz/src/main/java/com/example/vrpruebainterfaz/Registro.java
+++ VRPruebaInterfaz/src/main/java/com/example/vrpruebainterfaz/Registro.java
@@ -48,38 +48,70 @@ public class Registro {
         gridCampos.setHgap(15);
         gridCampos.setVgap(12);
 
+
+        Label section1 = new Label("Información Personal");
+        section1.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+
         //Personalización de campo nombre
         Label etiquetaName = new Label("Nombre");
         etiquetaName.setFont(Font.font("Arial", 15));
 
         TextField campoName = new TextField();
         campoName.setPrefWidth(200);
-        campoName.setPrefHeight(20);
+        campoName.setPrefHeight(25);
 
         //Personalización de campo apellido
-        Label etiquetaLastName = new Label("Apellido");
+        Label etiquetaLastName = new Label("Apellidos");
         etiquetaLastName.setFont(Font.font("Arial", 15));
 
         TextField campoLastName = new TextField();
         campoLastName.setPrefWidth(200);
-        campoLastName.setPrefHeight(20);
+        campoLastName.setPrefHeight(25);
+
+        //Personalización de campo correo institucional
+        Label etiquetaEmail = new Label("Correo Institucional");
+        etiquetaEmail.setFont(Font.font("Arial", 15));
+
+        TextField campoEmail = new TextField();
+        campoEmail.setPrefWidth(200);
+        campoEmail.setPrefHeight(25);
+
+
+
+        HBox buttons = new HBox(15);
+        buttons.setAlignment(Pos.CENTER);
 
         Button botonContinuar = new Button("Continuar");
         botonContinuar.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-        botonContinuar.setStyle("-fx-background-color: #0078d7; -fx-text-fill: white;");
-        botonContinuar.setPrefWidth(250);
-        botonContinuar.setPrefHeight(40);
+        botonContinuar.setStyle("-fx-background-color: #009475; -fx-text-fill: white;");
+        botonContinuar.setPrefWidth(200);
+        botonContinuar.setPrefHeight(30);
+
+        Button botonCancelar = new Button("Regresar");
+        botonCancelar.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        botonCancelar.setStyle("-fx-background-color: #005994; -fx-text-fill: white;");
+        botonCancelar.setPrefWidth(200);
+        botonCancelar.setPrefHeight(30);
+
+        botonCancelar.setOnAction(e -> {
+           HelloApplication regresar = new HelloApplication();
+           regresar.start(ventanaRegistro);
+        });
+
+        buttons.getChildren().addAll(botonContinuar, botonCancelar);
 
 
         //Etiquetas fila 0
         gridCampos.add(etiquetaName, 0, 0);
         gridCampos.add(etiquetaLastName, 1, 0);
+        gridCampos.add(etiquetaEmail, 2, 0);
 
         //TextField fila 0
         gridCampos.add(campoName, 0, 1);
         gridCampos.add(campoLastName, 1, 1);
+        gridCampos.add(campoEmail, 2, 1);
 
-        panelDerecho.getChildren().addAll(titulo, gridCampos, botonContinuar);
+        panelDerecho.getChildren().addAll(titulo, section1, gridCampos, buttons);
         contenedorPrincipal.getChildren().addAll(panelIzquierdo, panelDerecho);
 
         Scene escena = new Scene(contenedorPrincipal);
