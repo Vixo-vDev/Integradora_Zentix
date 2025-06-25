9f1e07d00496eb43a30030fcadfb70c5f7ab3b1e
Tony <20243ds055@utez.edu.mx>
2025-06-25 13:35:28 -0600

    VistaFormularios ejecutable

--- PanelAdmin/src/main/java/com/example/paneladmin/Vistas/VistaFormularios.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/Vistas/VistaFormularios.java
@@ -1,16 +1,38 @@
 package com.example.paneladmin.Vistas;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import com.example.paneladmin.Controladores.ControladorFormularios;
+import com.example.paneladmin.Modelos.Formulario;
+import javafx.geometry.Insets;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
-public class VistaFormularios extends Application {
+public class VistaFormularios {
+    private ControladorFormularios controlador;
+    private VBox vista;
+    private TableView<Formulario> tabla;
 
-    public static void main(String[] args) {
-        launch(args);
+    public VistaFormularios() {
+        vista = new VBox(20);
+        vista.setPadding(new Insets(20));
+        this.controlador = new ControladorFormularios(this); // Inicialización correcta del controlador
+        controlador.inicializarUI();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    public VBox getVista() {
+        return vista;
+    }
 
+    public TableView<Formulario> getTabla() {
+        return tabla;
+    }
+
+    public void setTabla(TableView<Formulario> tabla) {
+        this.tabla = tabla;
+        if (tabla != null) {
+            tabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+            tabla.setStyle("-fx-background-color: white; -fx-border-color: #bdc3c7;");
+            VBox.setVgrow(tabla, Priority.ALWAYS);
+        }
     }
 }
