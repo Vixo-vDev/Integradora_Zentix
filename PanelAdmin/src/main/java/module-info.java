d377b7b7ae258a791108ef6b5408bef041ef1113
Tony <20243ds055@utez.edu.mx>
2025-06-25 13:32:15 -0600

    Actulaisacion sin detalles

--- PanelAdmin/src/main/java/module-info.java
+++ PanelAdmin/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.paneladmin {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.paneladmin to javafx.fxml;
     exports com.example.paneladmin;
