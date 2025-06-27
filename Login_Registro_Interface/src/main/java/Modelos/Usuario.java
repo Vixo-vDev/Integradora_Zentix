9b5a857e5cd33ca262aefdf6e4572e359e677899
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-06-26 22:15:31 -0600

    Creación de contructores

--- Login_Registro_Interface/src/main/java/Modelos/Usuario.java
+++ Login_Registro_Interface/src/main/java/Modelos/Usuario.java
@@ -14,5 +14,19 @@ public class Usuario {
     private int edad;
     private String rol;
     private String password;
-    private String passwordConfirmar;
+
+    public Usuario(){}
+
+    public Usuario(String nombre, String apellidos, String correo, String calle, String lada, String telefono, String date, int edad, String rol, String password) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.calle = calle;
+        this.lada = lada;
+        this.telefono = telefono;
+        this.date = date;
+        this.edad = edad;
+        this.rol = rol;
+        this.password = password;
+    }
 }
