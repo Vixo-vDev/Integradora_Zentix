29a05a794fb4ed507a3a5204546cc2e941d57274
Tony <20243ds055@utez.edu.mx>
2025-06-26 19:32:15 -0600

    El inicialisador recibio nueva codificacion

--- PanelAdmin/src/main/java/com/example/paneladmin/HelloApplication.java
+++ PanelAdmin/src/main/java/com/example/paneladmin/HelloApplication.java
@@ -7,9 +7,8 @@ import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage escenarioPrimario) {
-        VistaPrincipal vistaPrincipal = new VistaPrincipal();
-        vistaPrincipal.mostrar(escenarioPrimario);
+    public void start(Stage primaryStage) {
+        new ControladorPrincipal(primaryStage);
     }
 
     public static void main(String[] args) {
