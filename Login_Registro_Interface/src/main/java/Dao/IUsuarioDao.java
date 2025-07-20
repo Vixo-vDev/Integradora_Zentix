1f2478845717049abbbcee5d74e9e2221cd1e65b
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-07-20 17:12:20 -0600

    Agregando clase interfaz de IUsario

---
+++ Login_Registro_Interface/src/main/java/Dao/IUsuarioDao.java
@@ -0,0 +1,18 @@
+package Dao;
+
+import com.example.netrixapp.Modelos.Usuario;
+
+import java.util.List;
+
+public interface IUsuarioDao {
+
+    //Método para login
+    public boolean login(String correo, String pass) throws Exception;
+
+    List<Usuario> findAll() throws Exception;
+    Usuario getById(int id) throws Exception;
+    void create(Usuario user) throws Exception;
+    void update(Usuario user) throws Exception;
+    void delete(Usuario user) throws Exception;
+
+}
