b095782a74b3b5585659ad086ea097526155af83
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-07-21 16:26:03 -0600

    Modelo equipo

---
+++ Login_Registro_Interface/src/main/java/com/example/netrixapp/Modelos/Equipo.java
@@ -0,0 +1,12 @@
+package com.example.netrixapp.Modelos;
+
+public class Equipo {
+    private int id_equipo;
+    private String codigo_bien;
+    private String descripcion;
+    private String marca;
+    private String modelo;
+    private String numero_serie;
+    private int disponible;
+    private int tipo_equipo;
+}
