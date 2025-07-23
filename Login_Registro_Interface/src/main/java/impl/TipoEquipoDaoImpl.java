a3dcacf7ff76c80cdca3d2fcd10c17463cc5d896
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-07-23 15:10:16 -0600

    Agregando método en TipoEquipoDaoImpl

--- Login_Registro_Interface/src/main/java/impl/TipoEquipoDaoImpl.java
+++ Login_Registro_Interface/src/main/java/impl/TipoEquipoDaoImpl.java
@@ -1,14 +1,34 @@
 package impl;
 
 import Dao.ITipoEquipoDao;
+import com.example.netrixapp.Controladores.ConnectionBD;
+import com.example.netrixapp.Modelos.TipoEquipo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TipoEquipoDaoImpl{
+public class TipoEquipoDaoImpl implements ITipoEquipoDao {
+    @Override
+    public List<TipoEquipo> obtenerTodos() {
+        List<TipoEquipo> lista = new ArrayList<>();
 
+        String sql = "SELECT * FROM TIPOEQUIPO";
+        try{
+            Connection con = ConnectionBD.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
 
+            if(rs.next()){
+                TipoEquipo tipoEquipo = new TipoEquipo();
+
+                tipoEquipo.setId_tipo_equipo(rs.getInt("id_tipo"));
+                tipoEquipo.setNombre(rs.getString("nombre"));
+                lista.add(tipoEquipo);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return lista;
+    }
 }
