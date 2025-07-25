e6d594c5f48195ed52b393e987b8c86ef22826f4
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-07-24 20:06:55 -0600

    Método enviarSolicitud

--- Login_Registro_Interface/src/main/java/com/example/netrixapp/Controladores/ControladorSolicitudes.java
+++ Login_Registro_Interface/src/main/java/com/example/netrixapp/Controladores/ControladorSolicitudes.java
@@ -1,19 +1,26 @@
 package com.example.netrixapp.Controladores;
 
+import com.example.netrixapp.Modelos.Solicitud;
+import com.example.netrixapp.Modelos.Usuario;
 import com.example.netrixapp.Vistas.VistaSolicitudes;
+import impl.SolicitudDaoImpl;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 
 import java.time.LocalDate;
 
 public class ControladorSolicitudes {
+
+    SolicitudDaoImpl solicitudDao = new SolicitudDaoImpl();
+    Usuario usuario = SesionUsuario.getUsuarioActual();
+    int id_usuario = usuario.getId_usuario();
     private VistaSolicitudes vista;
 
     //Declaracion de atributos
-    String nota = vista.getNota();
-    LocalDate fecha = vista.getFecha();
+    String razon = vista.getNota();
+    LocalDate fecha_solicitud = vista.getFecha();
     String tiempoUso = vista.getTiempoUso();
-    String equipos = vista.getEquipos();
+    String articulo = vista.getEquipos();
     int cantidad = vista.getCantidad();
 
     public ControladorSolicitudes(VistaSolicitudes vista) {
@@ -24,7 +31,7 @@ public class ControladorSolicitudes {
     }
 
     public boolean verificarCampos(){
-        if(nota.isEmpty() || fecha == null || tiempoUso.isEmpty() || equipos.isEmpty() || cantidad == 0){
+        if(razon.isEmpty() || fecha_solicitud == null || tiempoUso.isEmpty() || articulo.isEmpty() || cantidad == 0){
             Alert alerta = new Alert(Alert.AlertType.INFORMATION);
             alerta.setHeaderText("ERROR");
             alerta.setContentText("Hay campos sin completar");
@@ -47,7 +54,12 @@ public class ControladorSolicitudes {
     public void enviarSolicitud() {
         if(verificarCampos()){
             try{
-
+                Solicitud solicitud = new Solicitud(id_usuario,fecha_solicitud, articulo, cantidad, null, tiempoUso, razon, "Pendiente");
+                solicitudDao.create(solicitud);
+                Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+                alerta.setHeaderText("ÉXTIO");
+                alerta.setContentText("Solicitud enviada con éxito");
+                alerta.showAndWait();
             }catch(Exception e){
                 e.printStackTrace();
             }
